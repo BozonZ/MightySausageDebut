d59dd84f5d7a32f19af333c9b69717fba966b3e4
JoelQ <joelquey@hotmail.com>
2019-10-18 22:51:12 +0800

    Added Character Rotation
    It is wonky. Hope you guys could fix it.

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -1,11 +1,12 @@
-﻿ 
+﻿
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class CharacterMovement : MonoBehaviour
-{
+public class CharacterMovement : MonoBehaviour {
     [SerializeField] private float speed;
+    [SerializeField] private float turnRate;
     private Vector3 velocity;
+    private Vector3 rotation;
     private Rigidbody rb;
 
     private void Awake() {
@@ -13,7 +14,7 @@ public class CharacterMovement : MonoBehaviour
     }
 
     private void Update() {
-        
+
     }
 
     private void FixedUpdate() {
@@ -26,6 +27,8 @@ public class CharacterMovement : MonoBehaviour
         velocity.z = speed * Input.GetAxisRaw("Vertical");
         rb.velocity += velocity;
         rb.velocity = Vector3.ClampMagnitude(rb.velocity, speed);
+        rb.rotation = Quaternion.RotateTowards(rb.rotation, Quaternion.LookRotation(rotation, Vector3.up), turnRate * Time.deltaTime);
+        rotation = velocity.magnitude > 0.01f || velocity.magnitude < -0.01f ? velocity : rotation;
         velocity = Vector3.zero;
     }
 }
