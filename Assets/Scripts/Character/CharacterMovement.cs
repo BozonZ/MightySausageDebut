9df0fe609f12364d41b48d921059162f5d7d7e8c
JoelQ <joelquey@hotmail.com>
2019-10-18 21:51:27 +0800

    Added Character Movement

---
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,31 @@
+﻿ 
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Vector3 velocity;
+    private Rigidbody rb;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        
+    }
+
+    private void FixedUpdate() {
+        Move();
+    }
+
+    private void Move() {
+
+        velocity.x = speed * Input.GetAxisRaw("Horizontal");
+        velocity.z = speed * Input.GetAxisRaw("Vertical");
+        rb.velocity += velocity;
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, speed);
+        velocity = Vector3.zero;
+    }
+}
