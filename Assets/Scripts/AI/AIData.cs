1a248d2dac86ba7a826e9222970e6ecb35d71f50
Dreadevil <51286248+Rutto23@users.noreply.github.com>
2019-10-19 13:15:42 +0300

    Revert "test"
    This reverts commit b5288c3cd23dcc8e87f9215060a1b6b3d9de74de.

---
+++ Assets/Scripts/AI/AIData.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "AI", menuName = "AI/Create new AI", order = 1)]
+public class AIData : ScriptableObject
+{
+    [SerializeField] private new string name;
+    [SerializeField] private float health;
+    [SerializeField] private float damage;
+    [SerializeField] private float acceleration;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private float range;
+
+    public string Name => name;
+    public float Health => health;
+    public float Damage => damage;
+    public float Acceleration => acceleration;
+    public float MoveSpeed => moveSpeed;
+    public float RotateSpeed => rotateSpeed;
+    public float Range => range;
+}
+
+[System.Serializable]
+public struct AILevelData {
+    public Transform target;
+    public Wave[] waves;
+}
+
+[System.Serializable]
+public struct AISpawn {
+#if UNITY_EDITOR
+    [AIType, Tooltip("AI to spawn.")]
+#endif
+    public int ai;
+    [Tooltip("Amount of AI to spawn.")]
+    public int count;
+    [Tooltip("Spawn delay in batch")]
+    public float delay;
+    [Tooltip("Spawn interval in batch")]
+    public float interval;
+}
---
+++ Assets/Scripts/Receiver.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public abstract class Receiver : MonoBehaviour
+{
+    [SerializeField] protected Trigger trigger;
+
+    private void Start() {
+        trigger.OnTrigger += Activate;
+    }
+
+    protected abstract void Activate();
+}
