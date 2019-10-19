b5288c3cd23dcc8e87f9215060a1b6b3d9de74de
Dreadevil <51286248+Rutto23@users.noreply.github.com>
2019-10-19 13:14:23 +0300

    test

--- Assets/Scripts/AI/AIBatch.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "AIBatch", menuName = "AI/AI Wave System/Create new batch", order = 1)]
-public class AIBatch : ScriptableObject {
-    [Tooltip("Spawns in batch")]
-    public AISpawn[] aiSpawns;
-    [Tooltip("Batch delay in wave")]
-    public float delay;
-}
--- Assets/Scripts/AI/AIType.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-[ExecuteInEditMode, CreateAssetMenu(fileName = "AITypes", menuName = "AI/Create new AITypes", order = 0)]
-public class AIType : ScriptableObject {
-    public AI[] ais = null;
-}
